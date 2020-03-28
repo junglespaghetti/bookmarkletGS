95fb0a3c0f4ea132776b4b6e353fdd81931a8712
Glitch (bookmarlet-gs) <none>
2020-03-28 22:48:07 +0000

    💎🍦 Updated with Glitch

--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -1,12 +1,43 @@
-javascript: (function(s) {
-  gsUrl =
-    "https://script.google.com/macros/s/AKfycbxKmy8wnZSKN2i-VlcVwcUutTu_4bvHdZ7zU7yvbCN0PFyONN4/exec";
-  scriptParam = {
-    pass: "test",
-    callback: "jsonp_callback",
-    origin: location.hostname
+var jsonp_callback = function(data) {
+  alert(data.message);
+  let param = {
+    callback: "jsonp_callback2",
+    text: "and2"
   };
-  s = document.createElement("script");
-  s.src = gsUrl + "?pass=" + scriptParam.pass + "&loader=true";
-  document.body.appendChild(s);
-})();
+  loadJsonp(gsUrl, param);
+};
+
+var jsonp_callback2 = function(data) {
+  alert(data.message);
+};
+
+function showWindowFrame() {
+  var easyIndexedDB = jsPanel.create({
+    headerTitle: "Easy IndexedDB",
+    position: "center-top 0 80",
+    contentSize: "450 250",
+    content: '<div id="table-element"></div>',
+    callback: function(panel) {
+      showGridTable();
+    }
+  });
+}
+
+function showGridTable() {
+  var data = [
+    ["", "Ford", "Tesla", "Toyota", "Honda"],
+    ["2017", 10, 11, 12, 13],
+    ["2018", 20, 11, 14, 13],
+    ["2019", 30, 15, 12, 13]
+  ];
+
+  var container = document.getElementById("table-element");
+  var hot = new Handsontable(container, {
+    data: data,
+    rowHeaders: true,
+    colHeaders: true,
+    filters: true,
+    dropdownMenu: true,
+    licenseKey: "non-commercial-and-evaluation"
+  });
+}
