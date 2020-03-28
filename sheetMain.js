7e9ad39ac3e9b0c7e45fc62f17aa93751d6059a2
Glitch (bookmarlet-gs) <none>
2020-03-28 22:26:09 +0000

    🎮🚞 Updated with Glitch

--- sheetMain.js
+++ sheetMain.js
@@ -1,62 +1,60 @@
 function doGet(e) {
+  var text = e.parameter.text;
 
-    var text = e.parameter.text;
-  
-    Logger.log(e.parameter.callback);
+  Logger.log(e.parameter.callback);
 
-    var value;
+  var value;
 
-    if (text) {
-        value = "You say " + text;
-    } else {
-        value = "Please say something!";
-    }
+  if (text) {
+    value = "You say " + text;
+  } else {
+    value = "Please say something!";
+  }
 
-    var result = {
-        message: value
-    }
+  var result = {
+    message: value
+  };
 
-    var responseText;
+  var responseText;
 
-    var out = ContentService.createTextOutput();
+  var out = ContentService.createTextOutput();
 
-    var callback = e.parameter.callback;
-  
-  if(e.parameter.loader){
+  var callback = e.parameter.callback;
+
+  if (e.parameter.loader) {
     Logger.log(e.parameter.loader);
     Logger.log(getLoder());
     responseText = getLoder();
     out.setMimeType(ContentService.MimeType.JAVASCRIPT);
-    
-  }else{
-
+  } else {
     if (callback) {
-        responseText = callback + "(" + JSON.stringify(result) + ")";
-        //Mime Typeをapplication/javascriptに設定
-        out.setMimeType(ContentService.MimeType.JAVASCRIPT);
-
+      responseText = callback + "(" + JSON.stringify(result) + ")";
+      //Mime Typeをapplication/javascriptに設定
+      out.setMimeType(ContentService.MimeType.JAVASCRIPT);
     } else {
-        responseText = JSON.stringify(result);
-        //Mime Typeをapplication/jsonに設定
-        out.setMimeType(ContentService.MimeType.JSON);
+      responseText = JSON.stringify(result);
+      //Mime Typeをapplication/jsonに設定
+      out.setMimeType(ContentService.MimeType.JSON);
     }
-      
-    }
+  }
 
+  //JSONPテキストをセットする
+  out.setContent(responseText);
 
-    //JSONPテキストをセットする
-    out.setContent(responseText);
-
-    return out;
+  return out;
 }
 
-function getLoder(){
-  var loader = '(function(f,d,e,a,c,b){' +
-  'd=['+'"https://easy-indexeddb.glitch.me/js/dbBookmarklet.js"' +
-  '];e=['+  
-  '];for(a=0;a<e.length;a++)b=document.createElement("link"),b.type="text/css",b.rel="stylesheet",b.href=e[a],document.body.appendChild(b);for(a=0;a<d.length;a++)c=document.createElement("script"),c.src=d[a],a==d.length-1&&(c.onload=function(){f()}),document.body.appendChild(c)})' +
-  '(function(){loadJsonp(gsUrl,scriptParam)});';
-  loader += 'function loadJsonp(c,a){a.pass=a.pass?a.pass:scriptParam.pass;var b=document.createElement("script");b.src=c+objToParameter(a);document.body.appendChild(b)};';
-  loader += 'function objToParameter(a){if(a instanceof Object&&!(a instanceof Array)){var b=[];Object.keys(a).forEach(function(c){b.push(c+"="+a[c])});return"?"+b.join("&")}return""};';
- return loader;
+function getLoder() {
+  var loader =
+    "(function(f,d,e,a,c,b){" +
+    "d=[" +
+    '"https://easy-indexeddb.glitch.me/js/dbBookmarklet.js"' +
+    "];e=[" +
+    '];for(a=0;a<e.length;a++)b=document.createElement("link"),b.type="text/css",b.rel="stylesheet",b.href=e[a],document.body.appendChild(b);for(a=0;a<d.length;a++)c=document.createElement("script"),c.src=d[a],a==d.length-1&&(c.onload=function(){f()}),document.body.appendChild(c)})' +
+    "(function(){loadJsonp(gsUrl,scriptParam)});";
+  loader +=
+    'function loadJsonp(c,a){a.pass=a.pass?a.pass:scriptParam.pass;var b=document.createElement("script");b.src=c+objToParameter(a);document.body.appendChild(b)};';
+  loader +=
+    'function objToParameter(a){if(a instanceof Object&&!(a instanceof Array)){var b=[];Object.keys(a).forEach(function(c){b.push(c+"="+a[c])});return"?"+b.join("&")}return""};';
+  return loader;
 }
